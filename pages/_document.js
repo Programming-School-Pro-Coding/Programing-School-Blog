b826d8e757da774f63874d6f023d15d5f1b9a37c
Programing-School <mohamedehabam99@hotmail.com>
2022-01-24 12:49:38 +0200

    My Official Blog

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" />
+          <title>Programing-School | Blog</title>
+          <link rel="icon" href="./channels4_profile.jpg" />
+          <link rel="icon" href="../public/channels4_profile.jpg" />
+          <link rel="icon" href="/public/channels4_profile.jpg" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
